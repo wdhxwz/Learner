efcc45ae6c1bf7044301f44d019f3bf2b5d54a21
王东鸿 <donghong.wang@frontpay.cn>
2017-01-11 20:08:18 +0800

    123

--- wangdh.learner.base/src/main/java/com/wangdh/learner/base/json/JavaBeanSerializeToJson.java
+++ wangdh.learner.base/src/main/java/com/wangdh/learner/base/json/JavaBeanSerializeToJson.java
@@ -11,7 +11,6 @@ import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -54,17 +53,13 @@ public class JavaBeanSerializeToJson {
 
 		String jsonString = mapper.writeValueAsString(response);
 		String provinceJson = mapper.writeValueAsString(province);
-		// 序列化泛型
-		TypeReference<Province> typeReference2 = new TypeReference<Province>() {
-		};
-		Province p = Country.toJavaBean(provinceJson);
-		System.out.println(p.name);
 		System.out.println(jsonString);
 
 		// 序列化泛型
 		TypeReference<Response<Province>> typeReference = new TypeReference<Response<Province>>() {
 		};
 		Response<Province> object = mapper.readValue(jsonString, typeReference);
+
 		System.out.println(object.getSessionId());
 
 		System.out.println(mapper.writeValueAsString(country));
