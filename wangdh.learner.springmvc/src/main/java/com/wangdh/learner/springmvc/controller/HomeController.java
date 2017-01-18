f10208fb7063ac323cbd9831d76dca03e757f8c2
王东鸿 <1366678737@qq.com>
2017-01-18 22:49:32 +0800

    配置Spring MVC

---
+++ wangdh.learner.springmvc/src/main/java/com/wangdh/learner/springmvc/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.wangdh.learner.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+	@RequestMapping("/index")
+	public String index(){
+		return "home/index";
+	}
+	
+}
