bbfd68e8e543eb2e31ee467ea64fb67e069b8939
王东鸿 <donghong.wang@frontpay.cn>
2017-02-09 18:45:02 +0800

    321231132

--- wangdh.learner.rop/src/main/java/com/wangdh/learner/rop/manager/SampleAppSecretManager.java
+++ wangdh.learner.rop/src/main/java/com/wangdh/learner/rop/manager/SampleAppSecretManager.java
@@ -5,20 +5,20 @@ import java.util.Map;
 
 import com.rop.security.AppSecretManager;
 
-public class SampleAppSecretManager implements AppSecretManager{
+public class SampleAppSecretManager implements AppSecretManager {
 
 	private static Map<String, String> appKeySecretMap = new HashMap<String, String>();
 
-    static {
-        appKeySecretMap.put("00001", "abcdeabcdeabcdeabcdeabcde");
-        appKeySecretMap.put("00002","abcdeabcdeabcdeabcdeaaaaa");
-        appKeySecretMap.put("00003","abcdeabcdeabcdeabcdeaaaaa");
-    }
-    
+	static {
+		appKeySecretMap.put("00001", "abcdeabcdeabcdeabcdeabcde");
+		appKeySecretMap.put("00002", "abcdeabcdeabcdeabcdeaaaaa");
+		appKeySecretMap.put("00003", "abcdeabcdeabcdeabcdeaaaaa");
+	}
+
 	@Override
 	public String getSecret(String appKey) {
-		System.out.println("use SampleAppSecretManager!");
-        return appKeySecretMap.get(appKey);
+		System.out.println("appKey=" + appKey + ",secret=" + appKeySecretMap.get(appKey));
+		return appKeySecretMap.get(appKey);
 	}
 
 	@Override
