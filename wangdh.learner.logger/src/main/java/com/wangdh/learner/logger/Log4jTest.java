d3c30083a76ba3791852e4482ed6ce11325915dd
王东鸿 <donghong.wang@frontpay.cn>
2017-05-27 17:54:28 +0800

    rabbitmq

--- wangdh.learner.logger/src/main/java/com/wangdh/learner/logger/Log4jTest.java
+++ wangdh.learner.logger/src/main/java/com/wangdh/learner/logger/Log4jTest.java
@@ -1,5 +1,7 @@
 package com.wangdh.learner.logger;
 
+import org.apache.log4j.Logger;
+
 // import org.apache.log4j.Logger;
 
 /**
@@ -11,11 +13,18 @@ public class Log4jTest {
 	 * Log4j日志记录器
 	 */
 	// private static Logger logger = Logger.getLogger(Log4jTest.class);
+	
+	private static Logger logger = Logger.getLogger("com.wangdh.learner.logger.Log4jTest");  
 
 	public static void main(String[] args) {
+
+		logger.info("sbsbsb");
+		
 //		for (int i = 0; i < 220000000; i++) {
 //			logger.info("info" + i);
 //		}
 //		logger.info("info");
+		
+		System.out.println("OK");
 	}
 }
