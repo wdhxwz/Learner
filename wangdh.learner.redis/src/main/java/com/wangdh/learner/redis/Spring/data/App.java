f67314e58d04bbd0c19f9ff67395a8eaf98930f0
wdh <1366678737@qq.com>
2017-12-24 20:42:01 +0800

    ++

--- wangdh.learner.redis/src/main/java/com/wangdh/learner/redis/Spring/data/App.java
+++ wangdh.learner.redis/src/main/java/com/wangdh/learner/redis/Spring/data/App.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.BoundValueOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -21,6 +22,7 @@ public class App {
         // 直接通过连接进行操作
         JedisConnectionFactory jedisConnectionFactory = context.getBean(JedisConnectionFactory.class);
         RedisConnection redisConnection = jedisConnectionFactory.getConnection();
+        redisConnection.set("name".getBytes(),"wangdh".getBytes());
         byte[] value = redisConnection.get("name".getBytes());
         System.out.println(new String(value));
 
@@ -45,5 +47,13 @@ public class App {
         System.out.println(redisTemplate.getDefaultSerializer().getClass().getName());
         System.out.println(redisTemplate.getHashKeySerializer().getClass().getName());
         System.out.println(redisTemplate.getHashValueSerializer().getClass().getName());
+
+        // Bound Key:对指定key进行操作
+        BoundValueOperations<String,String> boundValueOps = redisTemplate.boundValueOps("name");
+        boundValueOps.append(" love");
+        System.out.println(boundValueOps.get());
+
+
+
     }
 }
