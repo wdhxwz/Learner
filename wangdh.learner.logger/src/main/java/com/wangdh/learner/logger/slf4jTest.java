0ced5a13f16ebc3a1822c90b48770ffcb996e49a
wangdh <1366678737@qq.com>
2018-07-09 15:31:48 +0800

    测试log的mdc功能

--- wangdh.learner.logger/src/main/java/com/wangdh/learner/logger/slf4jTest.java
+++ wangdh.learner.logger/src/main/java/com/wangdh/learner/logger/slf4jTest.java
@@ -2,13 +2,20 @@ package com.wangdh.learner.logger;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import java.util.UUID;
 
 public class slf4jTest {
 	private static Logger logger = LoggerFactory.getLogger(slf4jTest.class);
 	
 	
 	public static void main(String[] args) {
-		logger.info("info");
+		MDC.put("mdc_trace_id",UUID.randomUUID().toString());
+
+		logger.info("info1");
+		logger.info("info2");
+		logger.info("info3");
 
 		System.out.println("OK");
 	}
